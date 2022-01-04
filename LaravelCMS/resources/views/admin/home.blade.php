52af5a6a842fee67f0803fb97ecce04efd0a98c0
João Pedro Ferreira <joao.pedreira@estudante.ufjf.br>
2022-01-04 05:03:53 -0300

    Instalando e alterando algumas coisas no adminlte

--- LaravelCMS/resources/views/admin/home.blade.php
+++ LaravelCMS/resources/views/admin/home.blade.php
@@ -1,1 +1,25 @@
-Página inicial do admin
+@extends('adminlte::page')
+
+
+@section('title', 'Painel')
+
+@section('content_header')
+
+    <h1>Painel de Controle</h1>
+
+@endsection
+
+@section('content')
+
+    <h2>Conteúdo da minha pagina</h2>
+
+@endsection
+
+@section('css')
+    <!--<h3>Caso queira mudar o css de alguma coisa</h3>-->
+    <link rel="stylesheet" href="/assets/css/custom.css" />
+@endsection
+
+@section('js')
+    <script>alert("Rodando")</script>
+@endsection
