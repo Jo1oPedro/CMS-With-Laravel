be11af380a44002687e085bbbaa9d0a0491c72c4
João Pedro Ferreira <joao.pedreira@estudante.ufjf.br>
2022-01-04 04:28:03 -0300

    Definindo home usuario e home adm

--- LaravelCMS/routes/web.php
+++ LaravelCMS/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Site\HomeController;
+use App\Http\Controllers\Admin\AdminHomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index']);
+
+//Auth::routes();
+
+//Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin')->group(function() {
+
+    Route::get('/', [AdminHomeController::class, 'index']);
+
 });
