06e11c0c974941ceca9b899cc088ebcd5dce8061
João Pedro Ferreira <joao.pedreira@estudante.ufjf.br>
2022-01-06 05:42:46 -0300

    Arrumando paginação da index.blade de usuarios

--- LaravelCMS/app/Http/Controllers/UserController.php
+++ LaravelCMS/app/Http/Controllers/UserController.php
@@ -17,8 +17,9 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::all();
+        //$users = User::all();
 
+        $users = User::paginate(10);
         return view('admin.users.index', [
             'users' => $users
         ]);
