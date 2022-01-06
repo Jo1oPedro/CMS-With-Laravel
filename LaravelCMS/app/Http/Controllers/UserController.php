86a044ab921d3d4389d2975af669cd9a029ee1f6
João Pedro Ferreira <joao.pedreira@estudante.ufjf.br>
2022-01-06 07:21:30 -0300

    Fazendo alteração totalmente flexivel de usuarios

--- LaravelCMS/app/Http/Controllers/UserController.php
+++ LaravelCMS/app/Http/Controllers/UserController.php
@@ -91,7 +91,15 @@ class UserController extends Controller
      */
     public function edit($id)
     {
-        //
+        $user = User::find($id);
+
+        if($user) {
+            return view('admin.users.edit', [
+                'user' => $user
+            ]);
+        }
+     
+        return redirect('users.index');
     }
 
     /**
@@ -103,7 +111,103 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $user = User::find($id);
+
+        if($user) {
+
+            $data = $request->only( [
+                'name',
+                'email',
+                'password',
+                'password_confirmation'
+            ]);
+            if(!empty($data['name']) && !empty($data['email'])) {
+                $validator = Validator::make([
+                    'name' => $data['name'],
+                    'email' => $data['email'],
+                ], [
+                    'name' => ['required', 'string', 'max:100'],
+                    'email' => ['required', 'string', 'email', 'max:100']
+                ]);
+            } else if(!empty($data['name'])) {
+                $validator = Validator::make([
+                    'name' => $data['name'],
+                ], [
+                    'name' => ['required', 'string', 'max:100'],
+                ]);
+            } else if(!empty($data['email'])) {
+                $validator = Validator::make([
+                    'email' =>$data['email'],
+                ], [
+                    'email' => ['required', 'string', 'email','max:100']
+                ]); 
+            }
+
+            if(!empty($data['name']) || !empty($data['email'] || !empty($data['password']))) {
+                // 1. Alteração do nome
+                if(!empty($data['name'])) {
+                    $user->name = $data['name'];
+                }
+                // 2. Alteração do email
+                // 2.1 Primeiro, verificamos se o email foi alterado
+    
+                if(!empty($data['email']))
+                {
+                    if($user->email != $data['email']) {
+                        // 2.2 Verificamos se o novo email já existe
+                        $hasEmail = User::where('email', $data['email'])->get();
+                        // 2.3 Se não existir, nós alteramos
+                        if(count($hasEmail) === 0) {
+                            $user->email = $data['email'];
+                        } else {
+                            $validator->errors()->add('email', __('validation.unique', [
+                                'attribute' => 'email'
+                            ]));
+                        }
+                    } else {
+                        $validator->errors()->add('email', __('validation.unique', [
+                            'attribute' => 'email'
+                        ]));
+                    }
+                }
+                // 3. Alteração de senha
+                // 3.1 Verifica se o usuário digitou senha
+                if(!empty($data['password'])) {
+                    // 3.2 Verifica se a confirmação está ok
+                    if(strlen($data['password']) >= 4) {
+                        if($data['password'] === $data['password_confirmation']) {
+                            // 3.3 Altera a senha
+                            $user->password = Hash::make($data['password']);
+                        } else {
+                            $validator->errors()->add('password', __('validation.confirmed', [
+                                'attribute' => 'password'
+                            ]));
+                        }
+                    } else{
+                        $validator->errors()->add('password', __('validation.min.string', [
+                            'attribute' => 'password',
+                            'min' => 4
+                        ]));
+                    }
+                }
+            } else {
+                return redirect()->route('users.edit', [
+                    'user' => $id
+                ])->withErrors('É necessario preencher o campo nome, email ou senha para poder editar');
+            }
+            
+            if(isset($validator)) {
+                if(count($validator->errors()) > 0) {
+                    return redirect()->route('users.edit', [
+                        'user' => $id
+                    ])->withErrors($validator);
+                }
+            }
+
+            $user->save();
+        }
+
+        return redirect()->route('users.index');
     }
 
     /**
