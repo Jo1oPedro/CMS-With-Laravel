70bceb7fdf779e383574d9bda618a7ed137c244b
João Pedro Ferreira <joao.pedreira@estudante.ufjf.br>
2022-01-11 04:58:22 -0300

    Adicionando acesso de novos usuarios e não permitindo que mesmo ip conte mais de uma vez

--- LaravelCMS/app/Http/Controllers/Site/PagesController.php
+++ LaravelCMS/app/Http/Controllers/Site/PagesController.php
@@ -5,13 +5,23 @@ namespace App\Http\Controllers\Site;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Page;
+use App\Models\Visitor;
 
 class PagesController extends Controller
 {
-    public function index($slug) {
+
+    public function index(Request $request, $slug) {
         //$string = explode(" ", $slug);
         $page = Page::where('slug', $slug)->first();
         if($page) {
+            $tableVisitor = new Visitor;
+            $visitors = Visitor::find($request->ip());
+            if(!$visitors) {
+                $tableVisitor->ip = $request->ip();
+                $tableVisitor->date_access = date('Y-m-d H:i:s');
+                $tableVisitor->page = $slug;
+                $tableVisitor->save();
+            }
             return view('site.page', [
                 'page' => $page
             ]);
